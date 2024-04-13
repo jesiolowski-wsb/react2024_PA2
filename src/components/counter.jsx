391830cf02e742fc0ba74eea06c8617fda762678
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2024-04-13 16:23:15 +0200

    lists rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(5);
+  const [tagsList, setTagsList] = useState(["tag1", "tag2", "tag3"]);
 
   const formatCount = () => (count === 0 ? <span>Zero</span> : count);
 
@@ -10,6 +11,11 @@ const Counter = () => {
 
   return (
     <>
+      <ul>
+        {tagsList.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
       <span className={classes}>{formatCount()}</span>
       <button className="btn btn-secondary">increment</button>
     </>
