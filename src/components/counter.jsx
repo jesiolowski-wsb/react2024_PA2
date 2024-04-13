1dc8c4f6a36a04dc512e2328c7ca58c1e20ae633
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2024-04-13 16:15:27 +0200

    added logic to counter values + styling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
-  return <div>Hello world</div>;
+  const [count, setCount] = useState(5);
+
+  const formatCount = () => (count === 0 ? <span>Zero</span> : count);
+
+  let classes = "badge m-2 badge-";
+  classes += count === 0 ? "warning" : "primary";
+
+  return (
+    <>
+      <span className={classes}>{formatCount()}</span>
+      <button className="btn btn-secondary">increment</button>
+    </>
+  );
 };
 
 export default Counter;
