ee2972afa9fa0702ddf1521ed5d11a8a54351cd6
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2024-05-04 08:39:46 +0200

    count renamed to value

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,21 @@
 import React, { useState } from "react";
 
 const Counter = (props) => {
-  console.log("prosy przekazane do komponentu", props);
+  const [value, setValue] = useState(props.value);
 
-  const [count, setCount] = useState(0);
-
-  const formatCount = () => (count === 0 ? <span>Zero</span> : count);
+  const formatCount = () => (value === 0 ? <span>Zero</span> : value);
 
   let classes = "badge m-2 badge-";
-  classes += count === 0 ? "warning" : "primary";
+  classes += value === 0 ? "warning" : "primary";
 
   const handleIncrement = (incrementor) => {
-    setCount(count + incrementor);
+    setValue(value + incrementor);
   };
 
   return (
     <>
       <span className={classes}>{formatCount()}</span>
-      <button onClick={() => handleIncrement(10)} className="btn btn-secondary">
+      <button onClick={() => handleIncrement(1)} className="btn btn-secondary">
         increment
       </button>
     </>
